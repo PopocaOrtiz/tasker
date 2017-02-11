858b872a8881b4ad3cc7e84d10b9f25989359604
Fernando Popoca <popoca.ortiz@gmail.com>
2017-02-11 12:47:33 -0600

    reformateo codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,68 @@
-import { Component , Pipe , Injectable , PipeTransform } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2'
+import {Component, Pipe, Injectable, PipeTransform} from '@angular/core';
+import {AngularFire, FirebaseListObservable} from 'angularfire2'
 type estatusTarea = 'pendiente' | 'completa' | 'cancelada' | 'espera';
 
-export interface ITarea{
-  fecha : string
-  titulo : string
-  contenido : string
-  estatus : estatusTarea
-  tags : string[]
-  fechaCompletada ?:string
+export interface ITarea {
+    fecha: string
+    titulo: string
+    contenido: string
+    estatus: estatusTarea
+    tags: string[]
+    fechaCompletada ?: string
 }
 
 @Pipe({
-  name: 'filtroListado',
-  pure: false
+    name: 'filtroListado',
+    pure: false
 })
 @Injectable()
 export class FiltroListadoPipe implements PipeTransform {
-  transform(items: any[], palabra : string): any {
-    return items.filter(item =>{
-      if(!palabra)
-        return true;
-      return item.titulo.indexOf(palabra) !== -1 ||
-          item.contenido.indexOf(palabra) !== -1;
-    });
-  }
+    transform(items: any[], palabra: string): any {
+        return items.filter(item => {
+            if (!palabra)
+                return true;
+            return item.titulo.indexOf(palabra) !== -1 ||
+                item.contenido.indexOf(palabra) !== -1;
+        });
+    }
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  tareas : FirebaseListObservable<ITarea[]>;
+    tareas: FirebaseListObservable<ITarea[]>;
 
-  tareaSeleccionada : ITarea;
+    tareaSeleccionada: ITarea;
 
-  constructor(af : AngularFire){
-      this.tareas = af.database.list("/tareas");
-  }
-  nuevaTarea(){
-    let titulo = "";
-    if(titulo = prompt("Nombre")){
-      this.tareas.push({
-        fecha : 'hoy',
-        titulo : titulo,
-        contenido : "",
-        estatus : "pendiente",
-        tags : []
-      });
+    constructor(af: AngularFire) {
+        this.tareas = af.database.list("/tareas");
     }
-  }
 
-  seleccionarTarea(tarea : ITarea){
-    this.tareaSeleccionada = tarea;
-  }
-  cambiarEstatus(estatus : estatusTarea){
-    this.tareaSeleccionada.estatus = estatus;
-    if(estatus == 'completa'){
-      this.tareaSeleccionada.fechaCompletada = "Hoy";
+    nuevaTarea() {
+        let titulo = "";
+        if (titulo = prompt("Nombre")) {
+            this.tareas.push({
+                fecha: 'hoy',
+                titulo: titulo,
+                contenido: "",
+                estatus: "pendiente",
+                tags: []
+            });
+        }
     }
-  }
+
+    seleccionarTarea(tarea: ITarea) {
+        this.tareaSeleccionada = tarea;
+    }
+
+    cambiarEstatus(estatus: estatusTarea) {
+        this.tareaSeleccionada.estatus = estatus;
+        if (estatus == 'completa') {
+            this.tareaSeleccionada.fechaCompletada = "Hoy";
+        }
+    }
 }
